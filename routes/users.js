93f72f1619a1b9cb7b85e4435d6786715ca5d4e5
anubhab137 <adamsanubhab@gmail.com>
2020-12-19 18:24:29 +0530

    Changed CSS

--- routes/users.js
+++ routes/users.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('no user found');
+  res.render('users');
 });
 
 router.get('/cool',(req,res,next)=>{
